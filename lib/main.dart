4f1eb4b421c97edffaa0c028b3a931ddc70d4fa4
Bridget Baksa <bridget.baksa@gmail.com>
2021-03-05 16:39:49 -0500

    Adding flutter responsive framework

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_website/screens/home_page.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        BouncingScrollWrapper.builder(context, widget),
+        maxWidth: 1200,
+        minWidth: 450,
+        defaultScale: true,
+        breakpoints: [
+          ResponsiveBreakpoint.resize(450, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+          ResponsiveBreakpoint.autoScale(2460, name: '4K'),
+        ],
+      ),
+      title: 'Bridget Baksa',
       theme: ThemeData(
         primarySwatch: Colors.grey,
       ),
@@ -17,38 +30,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage();
-
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   // int _counter = 0;
-
-//   // void _incrementCounter() {
-//   //   setState(() {
-//   //     // This call to setState tells the Flutter framework that something has
-//   //     // changed in this State, which causes it to rerun the build method below
-//   //     // so that the display can reflect the updated values. If we changed
-//   //     // _counter without calling setState(), then the build method would not be
-//   //     // called again, and so nothing would appear to happen.
-//   //     _counter++;
-//   //   });
-//   // }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         decoration: BoxDecoration(
-//             image: DecorationImage(
-//           image: AssetImage('assets/images/background.jpg'),
-//           fit: BoxFit.cover,
-//         )),
-//       ),
-//     );
-//   }
-// }
